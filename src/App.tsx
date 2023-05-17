7d4aa3f449dde6edcda591c21c90cde44dfff220
Pawel Wujczyk <pwujczyk@hotmail.com>
2023-05-17 09:26:06 +0200

    passing object

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,24 @@ import React, { ReactElement, useState, useRef } from "react";
 
 import { PTButton, PTPlate } from "productivitytools.plate";
 
+const initialValue = (content: string) => [
+  {
+    type: "p",
+    children: [
+      {
+        text: content,
+      },
+    ],
+  } 
+];
+
 function App() {
-  const [content, setContent] = useState("Fdsdddda");
+  const [content, setContent] = useState(JSON.parse("[{\"type\":\"title\",\"children\":[{\"text\":\"121\"}]},{\"type\":\"unorderedList\",\"children\":[{\"type\":\"list-item\",\"children\":[{\"text\":\"\"},{\"type\":\"link\",\"href\":\"https://cloud.google.com/blog/topics/developers-practitioners/troubleshooting-cloud-functions-connection-issues-cloud-sql-private-ips/\",\"children\":[{\"text\":\"Blog \"}]},{\"text\":\"opublikowany\"}]},{\"type\":\"list-item\",\"children\":[{\"text\":\"\"},{\"type\":\"link\",\"href\":\"https://www.youtube.com/watch?time_continue=9&v=eSP4Y9fDDkQ&feature=emb_logo&themeRefresh=1\",\"children\":[{\"text\":\"Youtube \"}]},{\"text\":\"opublikowane\"}]},{\"type\":\"list-item\",\"children\":[{\"text\":\"Przeprowadziła się do nowego domu, \"}]},{\"type\":\"list-item\",\"children\":[{\"text\":\"Nie wie jak będzie się komunikować\"}]}]}]"));
+
+
 
   const setRawContent=(value:string)=>{
-    let x=(value);
+    let x=initialValue(value);
     setContent(x);
   }
 
