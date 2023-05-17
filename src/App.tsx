212b868921e4632d06c4eba243aedf513c1f5b76
Pawel Wujczyk <pwujczyk@hotmail.com>
2023-05-17 09:01:30 +0200

    updating string text working

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
+import React, { ReactElement, useState, useRef } from "react";
+
 import { PTButton, PTPlate } from "productivitytools.plate";
 
 function App() {
+  const [content, setContent] = useState("Fdsdddda");
+
+  const setRawContent=(value:string)=>{
+    let x=(value);
+    setContent(x);
+  }
+
   return (
     <div className="App">
       hello2
+      <input type="text" onChange={(x) => setRawContent(x.target.value)}></input>
       <PTButton label="click me!"></PTButton>
-      <PTPlate content="pawel"></PTPlate>
+      <PTPlate content={content}></PTPlate>
     </div>
   );
 }
